3ee95edacc54094268e2b95b0ae370ef5a955e9b
ruelgrajo <rgrajo@scentregroup.com>
2022-07-17 07:00:23 +0800

    Implement Splash Screen

---
+++ app/src/main/java/com/heliossoftwaredeveloper/ticketscanner/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.heliossoftwaredeveloper.ticketscanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Splash screen https://developer.android.com/guide/topics/ui/splash-screen/migrate
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
